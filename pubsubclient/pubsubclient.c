f74e2b03796f7f538a87380e30f04ff4ba125bcf
Jehiah Czebotar <jehiah@gmail.com>
2009-09-17 00:38:08 +0000

    straight pubsub client; tab->spaces fixes

--- pubsubclient/pubsubclient.c
+++ pubsubclient/pubsubclient.c
@@ -5,14 +5,15 @@
 #include <string.h>
 #include "event.h"
 #include "evhttp.h"
-#include "pubsub_to_pubsub.h"
+#include "pubsubclient.h"
 #define DEBUG 0
+#define BOUNDARY_LENGTH 14
+// \r\n--xXPubSubXx\r\n
 
+int source_callback_helper(struct evhttp_request *req, void *arg);
 
 struct global_data {
-    int (*cb)(char *data, struct evbuffer *evb, void *cbarg);
-    struct evhttp_connection *evhttp_target_connection;
-    char *target_address;
+    void (*cb)(char *data, void *cbarg);
     void *cbarg;
 };
 
@@ -28,42 +29,44 @@ http_post_done(struct evhttp_request *req, void *arg){
 
 void 
 source_callback (struct evhttp_request *req, void *arg){
+    while (source_callback_helper(req, arg) == 1){
+        int i = 1;
+    }
+}
+
+int 
+source_callback_helper(struct evhttp_request *req, void *arg){
+    if ((size_t)BOUNDARY_LENGTH > EVBUFFER_LENGTH(req->input_buffer)){
+        return 0;
+    }
     if (DEBUG) fprintf(stdout, "source_callback\n");
     // enum message_read_status done;
     evhttp_clear_headers(req->input_headers);
     evhttp_parse_headers(req, req->input_buffer);
     char *content_len;
     content_len = (char *) evhttp_find_header(req->input_headers, "Content-Length");
-    if (!content_len){return;}
+    if (!content_len){return 0; // drain buffer? // exit?
+        }
     
     int len = atoi(content_len);
     size_t len_size;
-    
     len_size = (size_t)len;
     if (DEBUG) fprintf(stdout, "received content_length:%d buffer has:%d\n", len, EVBUFFER_LENGTH(req->input_buffer));
 
     struct global_data *client_data = (struct global_data *)arg;
 
-    struct evhttp_request *evhttp_target_request = NULL;
-
-    evhttp_target_request = evhttp_request_new(http_post_done, NULL);
-    evhttp_add_header(evhttp_target_request->output_headers, "Host", client_data->target_address);
-
     char *data = calloc(len, sizeof(char *));
     evbuffer_remove(req->input_buffer, data, len);
     if (DEBUG)fprintf(stdout, "data has %d bytes\n", strlen(data));
     if (DEBUG)fprintf(stdout, "data=%s\n", data);
-    // empty buffer
-    evbuffer_drain(req->input_buffer, EVBUFFER_LENGTH(req->input_buffer));
-    // write to output buffer
-    int flag = (*client_data->cb)(data, evhttp_target_request->output_buffer, client_data->cbarg);
+    (*client_data->cb)(data, client_data->cbarg);
     free(data);
-    if (!flag){return;} // don't make the request
-
-    if (evhttp_make_request(client_data->evhttp_target_connection, evhttp_target_request, EVHTTP_REQ_POST, "/pub") == -1) {
-        fprintf(stdout, "FAILED make_request\n");
-        exit(1);
+    // empty buffer
+    // --
+    if ((size_t)BOUNDARY_LENGTH <= EVBUFFER_LENGTH(req->input_buffer) ){
+        evbuffer_drain(req->input_buffer, (size_t)BOUNDARY_LENGTH);
     }
+    return 1;
 }
 
 void
@@ -79,13 +82,12 @@ http_chunked_request_done(struct evhttp_request *req, void *arg)
 
 
 int
-pubsub_to_pubsub_main(char *source_address, int source_port, char *target_address, int target_port, int (*cb)(char *data, struct evbuffer *evb, void *arg), void *cbarg)
+pubsub_to_pubsub_main(char *source_address, int source_port, void (*cb)(char *data, void *arg), void *cbarg)
 {
 
     event_init();
 
     struct evhttp_connection *evhttp_source_connection = NULL;
-    struct evhttp_connection *evhttp_target_connection = NULL;
     struct evhttp_request *evhttp_source_request = NULL;
     
     fprintf(stdout, "connecting to http://%s:%d/sub\n", source_address, source_port);
@@ -95,17 +97,8 @@ pubsub_to_pubsub_main(char *source_address, int source_port, char *target_addres
         exit(1);
     }
 
-    fprintf(stdout, "connecting to http://%s:%d/pub\n", target_address, target_port);
-    evhttp_target_connection = evhttp_connection_new(target_address, target_port);
-    if (evhttp_target_connection == NULL) {
-        fprintf(stdout, "FAILED CONNECT TO TARGET %s:%d\n", target_address, target_port);
-        exit(1);
-    }
-
     struct global_data *data;
     data = calloc(1, sizeof(*data));
-    data->evhttp_target_connection = evhttp_target_connection;
-    data->target_address = target_address;
     data->cb = cb;
     data->cbarg = cbarg;
 
@@ -115,13 +108,12 @@ pubsub_to_pubsub_main(char *source_address, int source_port, char *target_addres
 
     if (evhttp_make_request(evhttp_source_connection, evhttp_source_request, EVHTTP_REQ_GET, "/sub") == -1) {
         fprintf(stdout, "FAILED make_request to source\n");
-        exit(1);
+        evhttp_connection_free(evhttp_source_connection);
+        return 1;
     }
 
     event_dispatch();
     evhttp_connection_free(evhttp_source_connection);
-    evhttp_connection_free(evhttp_target_connection);
     fprintf(stdout, "EXITING\n");
-
     return 0;
 }
---
+++ pubsubclient/pubsubclient.h
@@ -0,0 +1,3 @@
+
+int
+    pubsub_to_pubsub_main(char *source_address, int source_port, void (*cb)(char *data, void *arg), void *cbarg);
