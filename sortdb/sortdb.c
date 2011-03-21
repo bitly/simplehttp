bdd8bd68aeb250d7568ad239d28680735c3bc245
Jay Ridgeway <jayridge@gmail.com>
2011-03-21 15:05:59 -0400

    Added fwmatch api.

--- sortdb/sortdb.c
+++ sortdb/sortdb.c
@@ -101,7 +101,7 @@ void fwmatch_cb(struct evhttp_request *req, struct evbuffer *evb,void *ctx)
          * There's probably a better way to do this, however
          * this is easy and faults page in 4k chunks anyway.
          */
-        while (line != (char *)map_base && (prev = prev_line(line-1)) != line) {
+        while ((prev = prev_line(line)) != line) {
             if (strncmp(key, prev, keylen) != 0) break;
             line = prev;
         }
@@ -118,8 +118,7 @@ void fwmatch_cb(struct evhttp_request *req, struct evbuffer *evb,void *ctx)
         }
 
         if (end != start) {
-            evbuffer_add_reference(evb, (const void *)start,
-                                   (size_t)(end - start), NULL, NULL);
+            evbuffer_add(evb, start, (end - start));
         } else {
             evbuffer_add_printf(evb, "%s\n", line);
         }   
