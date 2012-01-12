44b2f258800412538026e6ef8b390925eeac9867
Micha Gorelick <micha@bit.ly>
2012-01-12 17:01:05 +0000

    Added new parameter to pub for multiple publishes in one call
    New parameter to /pub named "multipub".  It is an integer and represents
    the ascii code for which delineation to use.  So, if you are sending
    new-line delinated data, you would publish to:
    
    /pub?multipub=13
    
    And tab delineated would be
    
    /pub?multipub=9
    
    This argument defaults to 0 representing no delination. (ie: delination
    happens at the null character which is, by definition, at the end of
    the string)

--- pubsub/pubsub.c
+++ pubsub/pubsub.c
@@ -43,20 +43,20 @@ char *base64(const unsigned char *input, int length)
 {
     BIO *bmem, *b64;
     BUF_MEM *bptr;
-    
+
     b64 = BIO_new(BIO_f_base64());
     bmem = BIO_new(BIO_s_mem());
     b64 = BIO_push(b64, bmem);
     BIO_write(b64, input, length);
     BIO_flush(b64);
     BIO_get_mem_ptr(b64, &bptr);
-    
+
     char *buff = (char *)malloc(bptr->length);
     memcpy(buff, bptr->data, bptr->length-1);
     buff[bptr->length-1] = 0;
-    
+
     BIO_free_all(b64);
-    
+
     return buff;
 }
 
@@ -67,7 +67,7 @@ int is_slow(struct cli *client)
     }
     struct evhttp_connection *evcon;
     unsigned long output_buffer_length;
-    
+
     evcon = (struct evhttp_connection *)client->req->evcon;
     output_buffer_length = evcon->output_buffer ? (unsigned long)EVBUFFER_LENGTH(evcon->output_buffer) : 0;
     if (output_buffer_length > MAX_PENDING_DATA) {
@@ -104,24 +104,24 @@ void clients_cb(struct evhttp_request *req, struct evbuffer *evb, void *ctx)
     char buf[248];
     unsigned long output_buffer_length;
     struct evhttp_connection *evcon;
-    
+
     if (TAILQ_EMPTY(&clients)) {
         evbuffer_add_printf(evb, "no /sub connections\n");
     }
     TAILQ_FOREACH(client, &clients, entries) {
         evcon = (struct evhttp_connection *)client->req->evcon;
-        
+
         time_struct = gmtime(&client->connect_time);
         strftime(buf, 248, "%Y-%m-%d %H:%M:%S", time_struct);
         output_buffer_length = (unsigned long)EVBUFFER_LENGTH(evcon->output_buffer);
         evbuffer_add_printf(evb, "%s:%d connected at %s. output buffer size:%lu state:%d\n",
-                            client->req->remote_host,
-                            client->req->remote_port,
-                            buf,
-                            output_buffer_length,
-                            (int)evcon->state);
+                client->req->remote_host,
+                client->req->remote_port,
+                buf,
+                output_buffer_length,
+                (int)evcon->state);
     }
-    
+
     evhttp_send_reply(req, HTTP_OK, "OK", evb);
 }
 
@@ -131,7 +131,7 @@ void stats_cb(struct evhttp_request *req, struct evbuffer *evb, void *ctx)
     char buf[33];
     const char *reset;
     const char *format;
-    
+
     sprintf(buf, "%llu", totalConns);
     evhttp_add_header(req->output_headers, "X-PUBSUB-TOTAL-CONNECTIONS", buf);
     sprintf(buf, "%llu", currentConns);
@@ -142,10 +142,10 @@ void stats_cb(struct evhttp_request *req, struct evbuffer *evb, void *ctx)
     evhttp_add_header(req->output_headers, "X-PUBSUB-MESSAGES-SENT", buf);
     sprintf(buf, "%llu", kickedClients);
     evhttp_add_header(req->output_headers, "X-PUBSUB-KICKED-CLIENTS", buf);
-    
+
     evhttp_parse_query(req->uri, &args);
     format = (char *)evhttp_find_header(&args, "format");
-    
+
     if ((format != NULL) && (strcmp(format, "json") == 0)) {
         evbuffer_add_printf(evb, "{");
         evbuffer_add_printf(evb, "\"current_connections\": %llu,", currentConns);
@@ -161,13 +161,13 @@ void stats_cb(struct evhttp_request *req, struct evbuffer *evb, void *ctx)
         evbuffer_add_printf(evb, "Messages sent: %llu\n", msgSent);
         evbuffer_add_printf(evb, "Kicked clients: %llu\n", kickedClients);
     }
-    
+
     reset = (char *)evhttp_find_header(&args, "reset");
     if (reset) {
         msgRecv = 0;
         msgSent = 0;
     }
-    
+
     evhttp_send_reply(req, HTTP_OK, "OK", evb);
     evhttp_clear_headers(&args);
 }
@@ -175,7 +175,7 @@ void stats_cb(struct evhttp_request *req, struct evbuffer *evb, void *ctx)
 void on_close(struct evhttp_connection *evcon, void *ctx)
 {
     struct cli *client = (struct cli *)ctx;
-    
+
     if (client) {
         fprintf(stdout, "%llu >> close from  %s:%d\n", client->connection_id, evcon->address, evcon->port);
         currentConns--;
@@ -189,61 +189,83 @@ void on_close(struct evhttp_connection *evcon, void *ctx)
 
 void pub_cb(struct evhttp_request *req, struct evbuffer *evb, void *ctx)
 {
-    int i = 0;
+    int i = 0, j = 0;
     struct cli *client;
-    
-    msgRecv++;
-    totalConns++;
-    
-    TAILQ_FOREACH(client, &clients, entries) {
-        msgSent++;
-        evbuffer_drain(client->buf, EVBUFFER_LENGTH(client->buf));
-        if (is_slow(client)) {
-            if (can_kick(client)) {
-                evhttp_connection_free(client->req->evcon);
-                continue;
+    struct evkeyvalq args;
+    int message_length = 0;
+    int message_offset = 0;
+    int num_messages = 0;
+    char *current_message;
+    int multipub;
+
+    evhttp_parse_query(req->uri, &args);
+    multipub = get_int_argument(&args, "multipub", 0);
+
+    for (j=0; j<=EVBUFFER_LENGTH(req->input_buffer); j++)
+    {
+        if (j == EVBUFFER_LENGTH(req->input_buffer) || (multipub && *(EVBUFFER_DATA(req->input_buffer) + j) ==  (char)multipub))
+        {
+            message_length = j - message_offset ;
+            current_message = EVBUFFER_DATA(req->input_buffer) + message_offset;
+
+            msgRecv++;
+            totalConns++;
+
+            i = 0;
+            TAILQ_FOREACH(client, &clients, entries) {
+                msgSent++;
+                evbuffer_drain(client->buf, EVBUFFER_LENGTH(client->buf));
+                if (is_slow(client)) {
+                    if (can_kick(client)) {
+                        evhttp_connection_free(client->req->evcon);
+                        continue;
+                    }
+                    continue;
+                }
+                if (client->websocket) {
+                    // set to non-chunked so that send_reply_chunked doesn't add \r\n before/after this block
+                    client->req->chunked = 0;
+                    int ws_m = 0;
+                    int ws_message_length = message_length;
+                    int ws_frame_size = 64;  // Size for data fragmentation for websocket
+                    while (ws_m < ws_message_length) {
+                        int ws_cur_size = (ws_message_length - ws_m > ws_frame_size ? ws_frame_size : ws_message_length - ws_m);
+
+                        int ws_code = 0;
+                        if (ws_m == 0)
+                            ws_code += 0x01;
+                        if (ws_m + ws_cur_size >= ws_message_length)
+                            ws_code += 0x80;
+                        evbuffer_add_printf(client->buf, "%c", ws_code);
+
+                        evbuffer_add_printf(client->buf, "%c", ws_cur_size);
+                        evbuffer_add(client->buf, current_message + ws_m, ws_cur_size);
+                        ws_m += ws_cur_size;
+                    }
+
+                } else if (client->multipart) {
+                    /* chunked */
+                    evbuffer_add_printf(client->buf,
+                            "content-type: %s\r\ncontent-length: %d\r\n\r\n",
+                            "*/*",
+                            (int)message_length);
+                    evbuffer_add(client->buf, current_message, message_length);
+                    evbuffer_add_printf(client->buf, "\r\n--%s\r\n", BOUNDARY);
+                } else {
+                    /* new line terminated */
+                    evbuffer_add(client->buf, current_message, message_length);
+                    evbuffer_add_printf(client->buf, "\n");
+                }
+                evhttp_send_reply_chunk(client->req, client->buf);
+                i++;
             }
-            continue;
+
+            message_offset = j + 1;
+            num_messages ++;
         }
-        if (client->websocket) {
-            // set to non-chunked so that send_reply_chunked doesn't add \r\n before/after this block
-            client->req->chunked = 0;
-            int m = 0;
-            int message_length = EVBUFFER_LENGTH(req->input_buffer);
-            int frame_size = 64;  // Size for data fragmentation for websocket
-            while (m < message_length) {
-                int cur_size = (message_length - m > frame_size ? frame_size : message_length - m);
-
-                int code = 0;
-                if (m == 0)
-                    code += 0x01;
-                if (m + cur_size >= message_length)
-                    code += 0x80;
-                evbuffer_add_printf(client->buf, "%c", code);
-
-                evbuffer_add_printf(client->buf, "%c", cur_size);
-                evbuffer_add(client->buf, EVBUFFER_DATA(req->input_buffer)+m, cur_size);
-                m += cur_size;
-            }
-
-        } else if (client->multipart) {
-            /* chunked */
-            evbuffer_add_printf(client->buf,
-                                "content-type: %s\r\ncontent-length: %d\r\n\r\n",
-                                "*/*",
-                                (int)EVBUFFER_LENGTH(req->input_buffer));
-            evbuffer_add(client->buf, EVBUFFER_DATA(req->input_buffer), EVBUFFER_LENGTH(req->input_buffer));
-            evbuffer_add_printf(client->buf, "\r\n--%s\r\n", BOUNDARY);
-        } else {
-            /* new line terminated */
-            evbuffer_add(client->buf, EVBUFFER_DATA(req->input_buffer), EVBUFFER_LENGTH(req->input_buffer));
-            evbuffer_add_printf(client->buf, "\n");
-        }
-        evhttp_send_reply_chunk(client->req, client->buf);
-        i++;
     }
-    
-    evbuffer_add_printf(evb, "Published to %d clients.\n", i);
+
+    evbuffer_add_printf(evb, "Published %d messages to %d clients.\n", num_messages, i);
     evhttp_send_reply(req, HTTP_OK, "OK", evb);
 }
 
@@ -259,7 +281,7 @@ void sub_cb(struct evhttp_request *req, struct evbuffer *evb, void *ctx)
     char *host;
     char buf[248];
     struct tm *time_struct;
-    
+
     currentConns++;
     totalConns++;
     evhttp_parse_query(req->uri, &args);
@@ -271,24 +293,24 @@ void sub_cb(struct evhttp_request *req, struct evbuffer *evb, void *ctx)
     time_struct = gmtime(&client->connect_time);
     client->buf = evbuffer_new();
     client->kick_client = CLIENT_OK;
-    
+
     strftime(buf, 248, "%Y-%m-%d %H:%M:%S", time_struct);
-    
+
     // print out info about this connection
     fprintf(stdout, "%llu >> /sub connection from %s:%d %s\n", client->connection_id, req->remote_host, req->remote_port, buf);
-    
+
     // Connection: Upgrade
     // Upgrade: WebSocket
     ws_upgrade = (char *) evhttp_find_header(req->input_headers, "Upgrade");
     ws_origin = (char *) evhttp_find_header(req->input_headers, "Origin");
     ws_key = (char *) evhttp_find_header(req->input_headers, "Sec-WebSocket-Key");
     host = (char *) evhttp_find_header(req->input_headers, "Host");
-    
+
     if (ps_debug && ws_upgrade) {
         fprintf(stderr, "%llu >> upgrade header is %s\n", client->connection_id, ws_upgrade);
         fprintf(stderr, "%llu >> multipart is %d\n", client->connection_id, client->multipart);
     }
-    
+
     if (ws_upgrade && strcasestr(ws_upgrade, "WebSocket")) {
         if (ps_debug) {
             fprintf(stderr, "%llu >> upgrading connection to a websocket\n", client->connection_id);
@@ -325,11 +347,11 @@ void sub_cb(struct evhttp_request *req, struct evbuffer *evb, void *ctx)
         // evbuffer_add_printf(client->buf, "\r\n");
     } else if (client->multipart) {
         evhttp_add_header(client->req->output_headers, "content-type",
-                          "multipart/x-mixed-replace; boundary=" BOUNDARY);
+                "multipart/x-mixed-replace; boundary=" BOUNDARY);
         evbuffer_add_printf(client->buf, "--%s\r\n", BOUNDARY);
     } else {
         evhttp_add_header(client->req->output_headers, "content-type",
-                          "application/json");
+                "application/json");
         evbuffer_add_printf(client->buf, "\r\n");
     }
     if (client->websocket) {
@@ -356,11 +378,11 @@ int main(int argc, char **argv)
 
     define_simplehttp_options();
     option_define_bool("version", OPT_OPTIONAL, 0, NULL, version_cb, VERSION);
-    
+
     if (!option_parse_command_line(argc, argv)) {
         return 1;
     }
-    
+
     TAILQ_INIT(&clients);
     simplehttp_init();
     simplehttp_set_cb("/pub*", pub_cb, NULL);
@@ -369,6 +391,6 @@ int main(int argc, char **argv)
     simplehttp_set_cb("/clients", clients_cb, NULL);
     simplehttp_main();
     free_options();
-    
+
     return 0;
 }
