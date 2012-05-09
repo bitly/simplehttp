92fff217a847d674cf4de044d75a0ecaff27a900
Matt Reiferson <mreiferson@gmail.com>
2012-05-09 21:32:51 +0000

    update libqueuereader to support basic features:    * per-task retry    * retry_on and max_tries
    api change so that libqueuereader does the json parsing and passes a json_object along to callbacks

--- queuereader/queuereader.h
+++ queuereader/queuereader.h
@@ -1,25 +1,40 @@
 #ifndef __queuereader_h
 #define __queuereader_h
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <event.h>
+#include <evhttp.h>
+#include <signal.h>
+#include <time.h>
+#include <simplehttp/simplehttp.h>
+#include <json/json.h>
+
 #define QR_CONT                     0
 #define QR_EMPTY                    1
 #define QR_FAILURE                  2
 #define QR_SUCCESS                  3
 #define QR_CONT_SOURCE_REQUEST      4
 #define QR_REQUEUE_WITHOUT_BACKOFF  5
+#define QR_REQUEUE_WITHOUT_DELAY    6
 
-int queuereader_main(const char *source_address, int source_port, const char *path,
-                     int (*message_cb)(char *data, void *arg),
+
+int queuereader_main(struct json_object *tasks,
+                     const char *source_address, int source_port, const char *path,
+                     int (*message_cb)(struct json_object *json_msg, void *arg),
                      void (*error_cb)(int status_code, void *arg),
                      void *cbarg);
 void queuereader_run();
 void queuereader_free();
-void queuereader_init(const char *source_address, int source_port, const char *path,
-                      int (*message_cb)(char *data, void *arg),
+void queuereader_init(struct json_object *tasks,
+                      const char *source_address, int source_port, const char *path,
+                      int (*message_cb)(struct json_object *json_msg, void *arg),
                       void (*error_cb)(int status_code, void *arg),
                       void *cbarg);
 void queuereader_finish_message(int return_code);
 void queuereader_set_sleeptime_queue_empty_ms(int milliseconds);
-
+struct json_object *queuereader_copy_tasks(struct json_object *input_array);
+void queuereader_finish_task(int index);
 
 #endif
