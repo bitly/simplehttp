047be816e927555a9caf0e9340f53241db3f6f81
Pierce Lopez <pierce@bit.ly>
2012-05-17 17:25:36 +0000

    change (char *) to (const char *) where possible

--- simplehttp/options.h
+++ simplehttp/options.h
@@ -8,7 +8,7 @@ enum required_option {
 
 int option_parse_command_line(int argc, char **argv);
 int option_define_int(const char *option_name, int required, int default_val, int *dest, int(*cb)(int value), const char *help);
-int option_define_str(const char *option_name, int required, char *default_val, char **dest, int(*cb)(char *value), const char *help);
+int option_define_str(const char *option_name, int required, const char *default_val, char **dest, int(*cb)(char *value), const char *help);
 int option_define_bool(const char *option_name, int required, int default_val, int *dest, int(*cb)(int value), const char *help);
 int option_define_char(const char *option_name, int required, char default_val, char *dest, int(*cb)(char value), const char *help);
 
